b9a31615c2977552b634eed6c0900908033cc0ce
rileyg98 <rileyg98@users.noreply.github.com>
2019-12-10 20:30:24 +1000

    fix bugs in oidc

--- src/app/components/oidc.component.ts
+++ src/app/components/oidc.component.ts
@@ -9,22 +9,24 @@ export class OIDCComponent implements OnInit {
     oidcstate = '';
     oidccode = '';
     oidctok = '';
-    headers: new HttpHeaders({
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Authorization: Basic ': "NTgxODA1MDIzOTQ6ZmRlYjEzYzJhMzQzNDdhY2NjYTkxOWQzZjVhZmQ2MWMzZjM3ZmJlNjRmYzNlYTMyNTQ2ZDgxZGM"
-    })
-    constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) {}
+    headers;
+    snap;
+constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) { }
 
 
 ngOnInit() {
     this.snap = this.route.snapshot.params;
     this.oidcstate = this.snap.state;
     this.oidccode = this.snap.code;
+    headers: new HttpHeaders({
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization: Basic ': "NTgxODA1MDIzOTQ6ZmRlYjEzYzJhMzQzNDdhY2NjYTkxOWQzZjVhZmQ2MWMzZjM3ZmJlNjRmYzNlYTMyNTQ2ZDgxZGM"
+    });
         // TODO: get the oidc access token via code, then get user details and pre-fill the reg form (somehow getting a password!)
     this.http.post("https://api.vivokey.com/openid/token/", "redirect_uri=https://bitwarden.vivokey.com/oidc&grant_type=authorization_code&code=" + this.oidccode, this.headers)
         .subscribe(
             (jstok: string) => { this.oidctok = JSON.parse(jstok) },
-            () => { continue; },
+            () => { this.router.navigate("/")},
             () => {
                 this.router.navigate(["/login", "&access_token="+this.oidctok.access_token]);
             });
