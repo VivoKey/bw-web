b4cd4382fe605eaa0346ae2fe714ab7ad4bf5485
rileyg98 <rileyg98@users.noreply.github.com>
2019-12-10 21:51:42 +1000

    added toasts

--- src/app/components/oidc.component.ts
+++ src/app/components/oidc.component.ts
@@ -4,13 +4,15 @@ import {
 } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { I18nService } from 'jslib/abstractions/i18n.service';
+import { PlatformUtilsService } from 'jslib/abstractions/platformUtils.service';
 
 export class OIDCComponent implements OnInit {
     oidcstate = '';
     oidccode = '';
     oidctok = {"access_token":''};
     headers: {};
-constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient) { }
+    constructor(private route: ActivatedRoute, private router: Router, private http: HttpClient, protected i18nService: I18nService, protected platformUtilsService: PlatformUtilsService) { }
 
 
 ngOnInit() {
@@ -30,10 +32,15 @@ ngOnInit() {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Authorization': "Basic NTgxODA1MDIzOTQ6ZmRlYjEzYzJhMzQzNDdhY2NjYTkxOWQzZjVhZmQ2MWMzZjM3ZmJlNjRmYzNlYTMyNTQ2ZDgxZGM"
     });
-    this.http.post("https://api.vivokey.com/openid/token/", "redirect_uri=https://bitwarden.vivokey.com/%23/oidc&grant_type=authorization_code&code=" + this.oidccode, this.headers)
+    this.platformUtilsService.showToast('error', this.i18nService.t('errorOccurred'),
+        this.i18nService.t('emailRequired'));
+    this.http.post("https://api.vivokey.com/openid/token/", "?redirect_uri=https://bitwarden.vivokey.com/%23/oidc&grant_type=authorization_code&code=" + this.oidccode, this.headers)
         .subscribe(
             (jstok: string) => { this.oidctok = JSON.parse(jstok) },
-            () => { this.router.navigate(["/login"])},
+            () => { 
+                this.platformUtilsService.showToast('error', this.i18nService.t('errorOccurred'),
+                    this.i18nService.t('emailRequired'));
+                this.router.navigate(["/login"])},
             () => {
                 this.router.navigate(["/login", "&access_token="+this.oidctok.access_token]);
             });
