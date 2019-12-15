0ab25222243c5b20e5c8adb6d2a9ffd21f86e303
rileyg98 <rileyg98@users.noreply.github.com>
2019-12-15 17:46:38 +1000

    Fix typings
    Issues with typings and return values, attempting to fix

--- src/app/services/consumeoidc.service.ts
+++ src/app/services/consumeoidc.service.ts
@@ -8,10 +8,8 @@ import { LogService } from './log.service';
     providedIn: 'root',
 })
 export class ConsumeOIDCService {
-    oidctok: {'access_token': ''};
-    infotok: any;
-    infojs: any;
-    jstok: any;
+    oidctok: any;
+    
     tokencall: string;
 
     constructor(private http: HttpClient, private log: LogService) {
@@ -23,31 +21,34 @@ export class ConsumeOIDCService {
         headers = headers.set('Authorization', 'Basic MTA3MzI4Njg1ODI6M2MwOTAyNjQwOGRhODZkZTJmMTI0NTAyNGQ4YTFhMzE1MDIzNGE3ZDIzNjA1NDExNWQ5OGJlOTc=').set('Content-Type', 'application/x-www-form-urlencoded');
         this.tokencall = "redirect_uri=https://bitwarden.vivokey.com/%23/register&grant_type=authorization_code&code=".concat(token);
         try {
-            this.jstok = await this.http.post("https://api.vivokey.com/openid/token/", this.tokencall, {headers}).toPromise();
-            this.oidctok = JSON.parse(this.jstok);
+            let jstok: string = await this.http.post<string>("https://api.vivokey.com/openid/token/", this.tokencall, { headers }).toPromise();
+            this.oidctok = JSON.parse(jstok);
+
         } catch (err) {
             this.log.log(err);
             this.log.log(this.tokencall);
             this.log.log(headers);
+        } finally {
+            this.log.log(this.oidctok);
+            return this.oidctok;
         }
-        this.log.log(this.oidctok);
-        return this.oidctok;
+        
     }
 
     async getUserInfo(token: any) {
         let headers2 = new HttpHeaders();
         headers2 = headers2.set('Authorization', 'Bearer ' + token.access_token).set('Content-Type', 'application/x-www-form-urlencoded');
         try {
-            this.infotok = await this.http.post("https://api.vivokey.com/openid/userinfo/", {headers2}).toPromise();
-            this.infojs = JSON.parse(this.infotok);
+            let infotok = await this.http.post("https://api.vivokey.com/openid/userinfo/", {headers2}).toPromise();
+            let infojs = JSON.parse(infotok);
         } catch(err) {
         this.log.log(err);
         this.log.log(headers2);
         }
         return {
-            'name': this.infojs.full_name,
-            'email': this.infojs.email,
-            'sub': this.infojs.sub
+            'name': infojs.full_name,
+            'email': infojs.email,
+            'sub': infojs.sub
         };
     }
         
