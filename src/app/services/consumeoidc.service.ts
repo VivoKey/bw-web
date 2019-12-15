dfb75eb74ae3585f3a04191215057596959b0528
rileyg98 <rileyg98@users.noreply.github.com>
2019-12-15 17:12:00 +1000

    Update consumeoidc.service.ts

--- src/app/services/consumeoidc.service.ts
+++ src/app/services/consumeoidc.service.ts
@@ -20,7 +20,7 @@ export class ConsumeOIDCService {
 
     async getBearerToken(token: string) {
         let headers = new HttpHeaders();
-        headers = headers.set('Authorization: Basic MTA3MzI4Njg1ODI6M2MwOTAyNjQwOGRhODZkZTJmMTI0NTAyNGQ4YTFhMzE1MDIzNGE3ZDIzNjA1NDExNWQ5OGJlOTc=');
+        headers = headers.set('Authorization', 'Basic MTA3MzI4Njg1ODI6M2MwOTAyNjQwOGRhODZkZTJmMTI0NTAyNGQ4YTFhMzE1MDIzNGE3ZDIzNjA1NDExNWQ5OGJlOTc=');
         this.tokencall = "redirect_uri=https://bitwarden.vivokey.com/%23/register&grant_type=authorization_code&code=".concat(token);
         try {
             this.jstok = await this.http.post("https://api.vivokey.com/openid/token/", this.tokencall, {headers}).toPromise();
@@ -36,7 +36,7 @@ export class ConsumeOIDCService {
 
     async getUserInfo(token: any) {
         let headers2 = new HttpHeaders();
-        headers2 = headers2.set('Authorization: Bearer ' + token.access_token);
+        headers2 = headers2.set('Authorization', 'Bearer ' + token.access_token);
         try {
             this.infotok = await this.http.post("https://api.vivokey.com/openid/userinfo/", {headers2}).toPromise();
             this.infojs = JSON.parse(this.infotok);
