8d2f671f7b7f388d451f0ca79636534ebe329871
rileyg98 <rileyg98@users.noreply.github.com>
2019-12-15 17:51:52 +1000

    Update consumeoidc.service.ts

--- src/app/services/consumeoidc.service.ts
+++ src/app/services/consumeoidc.service.ts
@@ -40,12 +40,12 @@ export class ConsumeOIDCService {
         let headers2 = new HttpHeaders();
         headers2 = headers2.set('Authorization', 'Bearer ' + token.access_token).set('Content-Type', 'application/x-www-form-urlencoded');
         try {
-            let infotok = await this.http.post("https://api.vivokey.com/openid/userinfo/", {headers2}).toPromise();
+            let infotok = await this.http.post<string>("https://api.vivokey.com/openid/userinfo/", {headers2}).toPromise();
             let infojs = JSON.parse(infotok);
             this.userinfo = {
                 'name': infojs.full_name,
-                    'email': infojs.email,
-                        'sub': infojs.sub
+                'email': infojs.email,
+                'sub': infojs.sub
             };
         } catch(err) {
         this.log.log(err);
