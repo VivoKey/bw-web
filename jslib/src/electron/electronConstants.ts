05d78da5b9f778944f75e7200411ff78839f5f96
rileyg98 <rileyg98@users.noreply.github.com>
2020-05-04 13:50:40 +1000

    Re-import jslib

---
+++ jslib/src/electron/electronConstants.ts
@@ -0,0 +1,8 @@
+export class ElectronConstants {
+    static readonly enableMinimizeToTrayKey: string = 'enableMinimizeToTray';
+    static readonly enableCloseToTrayKey: string = 'enableCloseToTray';
+    static readonly enableTrayKey: string = 'enableTray';
+    static readonly enableStartToTrayKey: string = 'enableStartToTrayKey';
+    static readonly enableAlwaysOnTopKey: string = 'enableAlwaysOnTopKey';
+    static readonly minimizeOnCopyToClipboardKey: string = 'minimizeOnCopyToClipboardKey';
+}
