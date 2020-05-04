597925a0c48807733106510626319d31d2558a5b
rileyg98 <rileyg98@users.noreply.github.com>
2020-05-04 13:48:01 +1000

    Committing idk

---
+++ src/app/services/consumeoidc.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { LogService } from './log.service';
+
+
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ConsumeOIDCService {
+    
+    tokenresp: string;
+    tokencall: string;
+    userinfo: any;
+    private masterPass: string;
+    private email: string;
+    private name: string;
+    private info: boolean = false;
+
+    constructor(private http: HttpClient, private log: LogService) {
+        
+    }
+
+    async getBearerToken(token: string) {
+        // Pass-thru function due to rebuild of authentication
+        return token;
+        
+    }
+
+    async getUserInfo(token: string) {
+        
+        try {
+            let infotok = await this.http.get<any>("https://vault.vivokey.com/bwauth/webapi/getauth?code=" + token).toPromise();
+
+            this.userinfo = {
+                'name': infotok.name,
+                'email': infotok.email,
+                'passwd': infotok.passwd,
+                'new': infotok.new
+            };
+        } catch(err) {
+        }
+        return this.userinfo;
+    }
+   
+        
+
+
+
+}
